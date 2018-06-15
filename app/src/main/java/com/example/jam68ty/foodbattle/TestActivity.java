4e30e948d1b2a6d7e483ab8bdabb6a85ebd6c890
jiajanelee <tg0990475@gmail.com>
2018-06-15 18:03:18 +0800

    122

--- app/src/main/java/com/example/jam68ty/foodbattle/TestActivity.java
+++ app/src/main/java/com/example/jam68ty/foodbattle/TestActivity.java
@@ -2,8 +2,10 @@ package com.example.jam68ty.foodbattle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
